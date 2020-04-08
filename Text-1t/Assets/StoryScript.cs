9f73d01d00fb61d4b40b4da9d55c6a10743c25d9
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-08 20:56:32 +0300

    Update

---
+++ Text-1t/Assets/StoryScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class StoryScript : MonoBehaviour
+{
+    [SerializeField]
+    Text textComponent;
+
+
+    void Start()
+    {
+        textComponent.text = "Start function initial text!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
