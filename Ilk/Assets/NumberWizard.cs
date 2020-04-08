ecccd37f4b2e66d4f6f67af76d674c6627db4491
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-08 18:11:34 +0300

    Update

--- Ilk/Assets/NumberWizard.cs
+++ Ilk/Assets/NumberWizard.cs
@@ -13,6 +13,9 @@ public class NumberWizard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("yukari basildi");
+        }
     }
 }
