2bae702f7befc05dd6f13e31558f10a26c59e280
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-10 19:49:12 +0300

    update

--- Text-1t/Assets/State.cs
+++ Text-1t/Assets/State.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class State : ScriptableObject
 {
     [TextArea(5, 10)] [SerializeField] private string storyText;
+    [SerializeField] private State[] nextState;
 
     public string GetStateStory()
     {
         return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextState;
     }
 }
