90853ca475f07fd1afcb4fd8c742de3ae262c24e
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-10 20:10:53 +0300

    update

--- Text-1t/Assets/StoryScript.cs
+++ Text-1t/Assets/StoryScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine.UI;
 using UnityEngine;
+using System;
 
 public class StoryScript : MonoBehaviour
 {
@@ -10,6 +11,27 @@ public class StoryScript : MonoBehaviour
     void Start()
     {
         state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    private void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        State[] nextState = state.GetNextStates();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextState[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextState[1];
+        } 
+
         textComponent.text = state.GetStateStory();
     }
 }
