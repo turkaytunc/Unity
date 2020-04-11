9c92eb2c94bb82ab25e94238fd6f4d59932434d2
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-11 20:52:51 +0300

    update

---
+++ Guess-It/Assets/ChangeScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public void ChangeSceneFunction()
+    {
+        SceneManager.LoadScene("Scene001");
+    }
+}
