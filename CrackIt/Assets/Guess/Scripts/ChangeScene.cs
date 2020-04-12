f70d9c5f019e2a312d51b59cde9e84251f90bb5e
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-12 20:58:37 +0300

    update

---
+++ CrackIt/Assets/Guess/Scripts/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] private string nextScene;
+    public void ChangeSceneFunction()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
