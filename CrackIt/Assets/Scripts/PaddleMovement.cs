9bfd7abcbff9c57bf54cc73c6c7d32c6743217e7
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-13 02:53:05 +0300

    update

---
+++ CrackIt/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+
+    void Update()
+    {
+        float currentMousePosition = Input.mousePosition.x / Screen.width * 16;
+        Debug.Log(currentMousePosition);
+        Vector2 paddlePosition = new Vector2(currentMousePosition, transform.position.y);
+
+        transform.position = paddlePosition;
+    }
+}
