6cce008418b2dab944161a1c77cb644950ea9938
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-13 16:54:16 +0300

    update

--- CrackIt/Assets/Scripts/Block.cs
+++ CrackIt/Assets/Scripts/Block.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private GameObject tmPro;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        tmPro.GetComponent<Score>().currentScore += 1;
         Destroy(gameObject);
     }
 }
---
+++ CrackIt/Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    TextMeshProUGUI textMesh;
+    public int currentScore = 0;
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.text = "Score: " + currentScore.ToString();
+    }
+}
