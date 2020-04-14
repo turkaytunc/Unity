5687dcb24dffba48475fe92ba55de5943c9ddfc9
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-15 00:17:26 +0300

    update

--- CrackIt/Assets/Scripts/Block.cs
+++ CrackIt/Assets/Scripts/Block.cs
@@ -5,10 +5,12 @@ public class Block : MonoBehaviour
 {
     [SerializeField] private GameObject tmPro;
     [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private GameObject particle;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(audioClips[Random.Range(0, audioClips.Length)], Camera.main.transform.position, 0.5f);
+        Instantiate(particle, transform.position, transform.rotation);
         tmPro.GetComponent<Score>().currentScore += 1;
         Destroy(gameObject);
     }
