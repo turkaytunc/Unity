4a959460339714ec3ea92c9fee712677e5bad7f2
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-15 00:24:20 +0300

    update

---
+++ CrackIt/Assets/Scripts/DestroySelf.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1f);
+    }
+
+}
