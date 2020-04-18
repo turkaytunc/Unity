8c58b95597ce0166f7bf248f9e154e3a679cc860
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-19 00:08:35 +0300

    update

---
+++ SpaceShooter/Assets/Scripts/Laser.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    [SerializeField] private float laserVelocity = 20f;
+
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        rigidbody2D.velocity = new Vector2(0, laserVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
