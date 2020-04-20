aeb49dc81db508073dede508b8876776700afe33
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-20 23:34:24 +0300

    update

---
+++ SpaceShooter/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] private List<Transform> waypoints;
+    private int waypointIndex = 0;
+
+
+    void Update()
+    {
+        
+    }
+}
