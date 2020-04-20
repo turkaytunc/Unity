85a13eaffec80c725e002c0a657bb62b980019f7
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-21 00:31:36 +0300

    update

--- SpaceShooter/Assets/Scripts/WaveConfig.cs
+++ SpaceShooter/Assets/Scripts/WaveConfig.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(menuName = "EnemyConfig")]
 public class WaveConfig : ScriptableObject
@@ -12,4 +10,11 @@ public class WaveConfig : ScriptableObject
     [SerializeField] private float spawnRandomizerTime = 0.3f;
     [SerializeField] private int numberOfEnemies = 5;
     [SerializeField] private float moveSpeed = 5f;
+
+    public GameObject EnemyPrefab { get => enemyPrefab; }
+    public GameObject PathPrefab { get => pathPrefab;}
+    public float TimeBetweenSpawns { get => timeBetweenSpawns;}
+    public float SpawnRandomizerTime { get => spawnRandomizerTime;}
+    public int NumberOfEnemies { get => numberOfEnemies;}
+    public float MoveSpeed { get => moveSpeed;}
 }
