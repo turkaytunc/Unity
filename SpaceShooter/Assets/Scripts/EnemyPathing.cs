6cf9946e01562fda1046be4410871924fdea3699
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-20 23:54:16 +0300

    update

--- SpaceShooter/Assets/Scripts/EnemyPathing.cs
+++ SpaceShooter/Assets/Scripts/EnemyPathing.cs
@@ -1,15 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] private List<Transform> waypoints;
+    [SerializeField] private float movementSpeed = 5f;
     private int waypointIndex = 0;
 
 
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].position, movementSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, waypoints[waypointIndex].position) < 0.3f)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
