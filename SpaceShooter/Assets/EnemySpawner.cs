cfc23b2d3ff7d9bc81be4b39c4d5b94420e12454
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-21 01:18:56 +0300

    update

--- SpaceShooter/Assets/EnemySpawner.cs
+++ SpaceShooter/Assets/EnemySpawner.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<WaveConfig> waveConfigs;
-    
+    int startingWave = 0;
 
     void Start()
     {
+        WaveConfig currentWave = waveConfigs[startingWave];
+
+        StartCoroutine(SpawnAllEnemies(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemies(WaveConfig currentWave)
+    {
+
+        for(int i = 0; i <= currentWave.NumberOfEnemies; i++)
+        {
+            Instantiate(currentWave.EnemyPrefab, currentWave.GetWayPoints()[0].position, Quaternion.identity);
+            yield return new WaitForSeconds(currentWave.TimeBetweenSpawns);
         
+        }
+
     }
 
     // Update is called once per frame
