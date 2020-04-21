30a0ab865eba2afc9dcfa3b082289ecafbe3f2d2
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-21 15:17:49 +0300

    update

--- SpaceShooter/Assets/Scripts/Laser.cs
+++ SpaceShooter/Assets/Scripts/Laser.cs
@@ -15,12 +15,6 @@ public class Laser : MonoBehaviour
         Destroy(gameObject, 4f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject);
