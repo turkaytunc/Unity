787101227331ed46df712656861b6124e4699e9d
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-21 15:48:11 +0300

    update

---
+++ SpaceShooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float health = 500;
+    [SerializeField] private GameObject explosionPrefab;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int damage = collision.gameObject.GetComponent<DamageDealer>().GetDamage();
+
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
